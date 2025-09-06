d7547ca6b406653a15e4bb72fa410e39ef9da952
Vlad-Vasinev <vda5petr@rambler.ru>
2025-09-06 23:02:53 +0300

    fix: Fixes logo

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Image from 'next/image';
+import rsLogo from '../../assets/rss-logo.svg';
 
 const Footer = () => {
   return (
@@ -15,12 +16,7 @@ const Footer = () => {
       </a>
       <p className="h4">2025</p>
       <a rel="noreferrer" target="_blank" href="https://wearecommunity.io/events/rs-react-2025q3">
-        <Image
-          src="https://avatars.githubusercontent.com/u/11501370?s=280&v=4"
-          alt="404 error"
-          width={1200}
-          height={600}
-        />
+        <Image src={rsLogo} alt="404 error" width={1200} height={600} />
       </a>
     </footer>
   );
