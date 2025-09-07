f2fd89b56c4e34889bb0e620a57214600cd92765
KirrBrest <lukashchuk.kiryl@yandex.by>
2025-09-08 01:07:25 +0300

    feat: Create new tests for layout  (Mon, Sep 08, 2025 01:07:25 AM)

--- app/__tests__/page.test.tsx
+++ app/__tests__/page.test.tsx
@@ -1,17 +1,21 @@
 import { render, screen } from '@testing-library/react';
 import { describe, it, expect } from 'vitest';
 import Home from '../page';
+import MainLayout from '../components/MainLayout';
 
 describe('Home Page', () => {
   it('renders welcome message', () => {
     render(<Home />);
 
-    // expect(screen.getByText('Welcome to Next.js')).toBeInTheDocument();
     expect(screen.getByText(/Postman clone is here/)).toBeInTheDocument();
   });
 
   it('renders with correct structure', () => {
-    render(<Home />);
+    render(
+      <MainLayout>
+        <Home />
+      </MainLayout>
+    );
 
     expect(screen.getByTestId('app')).toBeInTheDocument();
     expect(screen.getByTestId('header')).toBeInTheDocument();
