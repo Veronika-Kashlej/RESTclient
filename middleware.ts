f3654cd52cf1daf0edaf7916d1d11f59fd5163cd
KirrBrest <lukashchuk.kiryl@yandex.by>
2025-09-21 13:52:01 +0300

    fix: Fix some bugs in url (Sun, Sep 21, 2025 01:52:01 PM)

--- middleware.ts
+++ middleware.ts
@@ -10,5 +10,5 @@ export default createMiddleware({
 });
 
 export const config = {
-  matcher: ['/', '/(en|ru|es)/:path*', '/((?!_next|_vercel|.*\\..*).*)'],
+  matcher: ['/', '/(en|ru|es)/:path*', '/((?!_next|_vercel|api|.*\\..*).*)'],
 };
